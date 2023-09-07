9d6921f0b0ac50aa4bfc1ca952a7d87c1f2be9c8
kirillkondratev <125269901+kirillkondratev@users.noreply.github.com>
2023-09-07 23:44:53 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,21 +1,24 @@
 with tab as (
-select distinct on (visitor_id)
-	visitor_id,
-	visit_date,
-	source,
-	medium,
-	campaign,
-	lead_id,
-	created_at,
-	amount,
-	closing_reason,
-	status_id
-from sessions 
-left join leads
-using(visitor_id)
+    select distinct on (visitor_id)
+        visitor_id,
+        visit_date,
+        source,
+        medium,
+        campaign,
+        lead_id,
+        created_at,
+        amount,
+        closing_reason,
+        status_id
+    from sessions
+    left join leads
+        on sessions.visitor_id = leads.visitor_id
 where medium != 'organic'
-order by visitor_id, visit_date desc)
+order by visitor_id asc, visit_date desc
+)
 
 select * from tab
 where created_at > visit_date or created_at is null
-order by amount desc nulls last
+order by
+amount desc nulls last, visit_date asc, source asc, medium asc, campaign asc;
+
